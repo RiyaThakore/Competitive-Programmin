979e4495e24616e7e5ae00e669b0fee38e6dfd4c
RiyaThakore <30748253+RiyaThakore@users.noreply.github.com>
2021-02-12 17:33:48 +0530

    Create get-minimum-element.py

---
+++ Stack and Queue/get-minimum-element.py
@@ -0,0 +1,33 @@
+class minStack:
+  minElement=float('inf')
+
+  def __init__(self):
+    self.min=float('inf')
+    self.stack=[]
+  
+  def push(self, x):
+    if x<=self.min:
+      self.stack.append(self.min)
+      self.min=x
+    self.stack.append(self.min)
+
+  def pop(self):
+    t=self.stack[-1]
+    self.stack.pop()
+    if self.min == t:
+      self.min = self.stack[-1]
+      self.stack.pop()
+
+  def top(self):
+      return self.stack[-1]
+
+  def getMin(self):
+      return self.min
+m = minStack()
+m.push(-2)
+m.push(0)
+m.push(-3)
+print(m.getMin())
+m.pop()
+print(m.top())
+print(m.getMin())
